44f56f1c9796d3f05b9d57569bc04b43e2ebf4b0
Oleg Andreev <oleganza@gmail.com>
2008-09-10 14:55:31 +0200

    spec for pids/protocol connection refusal

--- spec/pids/protocol_spec.rb
+++ spec/pids/protocol_spec.rb
@@ -5,42 +5,43 @@ describe Protocol do
   before(:each) do
     @protocol_class = Class.new{include Protocol}
     @connection = @protocol_class.new
+    
+    #
+    # Mock stubs
+    #
+    @local_uuid = local_uuid = "local-uuid"
+    @remote_uuid = remote_uuid = "remote-uuid"
+    
+    @connection.stub!(:send_marshalled_message).and_return(nil)
+    @local_pid = local_pid = mock("Pid", :uuid => @local_uuid, 
+                                         :options => {:uuid => @local_uuid})
+    local_pid.instance_eval do
+      stub!(:_register_pid).and_return(nil)
+      stub!(:connected).and_return(nil)
+    end
+    
+    @remote_pid = remote_pid = mock("RemotePid", :uuid => @remote_uuid, 
+                                                 :options => {:uuid => @remote_uuid})
   end
   
-  describe "with successful connecting scenario" do
+  describe "successful connection" do
     
     before(:each) do
-      #
-      # Mock stubs
-      #
-      @local_uuid = local_uuid = "local-uuid"
-      @remote_uuid = remote_uuid = "remote-uuid"
-      
-      @connection.stub!(:send_marshalled_message).and_return(nil)
-      @local_pid = local_pid = mock("Pid", :uuid => @local_uuid, 
-                                           :options => {:uuid => @local_uuid})
-      local_pid.instance_eval do
-        stub!(:_register_pid).and_return(nil)
-        stub!(:connected).and_return(nil)
-      end
-      
-      @remote_pid = remote_pid = mock("RemotePid", :uuid => @remote_uuid, 
-                                                   :options => {:uuid => @remote_uuid})
       #
       # Mock expectations
       #
       rpid = duck_type(:uuid, :_connection, :options)
-      local_pid.should_receive(:_register_pid).once.with(rpid)
-      local_pid.should_receive(:connected).once.with(rpid)
+      @local_pid.should_receive(:_register_pid).once.with(rpid)
+      @local_pid.should_receive(:connected).once.with(rpid)
       
       #
       # Init
       #
       @connection.address = em_addr.parsed_uri
-      @connection.local_pid = local_pid
+      @connection.local_pid = @local_pid
       @connection.post_init
       @connection.connection_completed
-      @connection.receive_marshalled_message([:hello, remote_pid.options])
+      @connection.receive_marshalled_message([:hello, @remote_pid.options])
       @rpid = @connection.remote_pid
     end
     
@@ -57,4 +58,30 @@ describe Protocol do
     end
     
   end
+  
+  
+  describe "connection refusal" do
+    
+    before(:each) do
+      #
+      # Mock expectations
+      #
+      @local_pid.should_receive(:connecting_failed).once.with(@connection)
+      
+      #
+      # Init
+      #
+      @connection.address = em_addr.parsed_uri
+      @connection.local_pid = @local_pid
+      @connection.post_init
+      @connection.unbind
+      @rpid = @connection.remote_pid
+    end
+    
+    it "should not have #remote_pid" do
+      @rpid.should be_nil
+    end
+    
+  end
+  
 end
