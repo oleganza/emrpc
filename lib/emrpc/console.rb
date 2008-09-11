451d14f21505cb268c0ac5edada01bdeead1bdab
Oleg Andreev <oleganza@gmail.com>
2008-09-11 11:58:40 +0200

    empowered console with gem_console

--- lib/emrpc/console.rb
+++ lib/emrpc/console.rb
@@ -1,7 +1,5 @@
-require 'irb'
-
 # Runs eventmachine reactor in a child thread,
-# waits 0.5 sec. in a current thread.
+# waits while reactor is ready in the current thread.
 # Returns child thread.
 #
 module EventMachine
@@ -17,24 +15,16 @@ end
 
 module EMRPC
   module Console
-    def self.included(klass)
-      klass.module_eval do
-
-        def setup
-          $em_thread ||= EventMachine.run_in_thread
-        end
-
-        def help!
-          puts ("
-            n/a yet
-            ".unindent!)
-        end
-
-      end
-      klass.send(:include, EMRPC)
-      klass.send(:setup)
-
-      puts "EMRPC #{::EMRPC::VERSION} (help! for more info)"
-    end # self.included
+    include GemConsole
+    help 'n/a yet'
+    
+    def project_name
+      "EMRPC #{::EMRPC::VERSION}"
+    end
+    
+    def setup
+      $em_thread ||= EventMachine.run_in_thread(0)
+    end
+    
   end # Console
 end # EMRPC
