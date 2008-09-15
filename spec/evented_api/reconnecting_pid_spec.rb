f6f09834d7cd6cdc92c4a835da789bb01e5f5e94
Oleg Andreev <oleganza@gmail.com>
2008-09-15 13:29:35 +0200

    specs for reconnecting pid

--- spec/evented_api/reconnecting_pid_spec.rb
+++ spec/evented_api/reconnecting_pid_spec.rb
@@ -1,6 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe ReconnectingPid do
+  extend MockDescribe
   
   before(:each) do
     @options = {
@@ -11,68 +12,65 @@ describe ReconnectingPid do
     }
     @connection = mock('connection')
     @remote = mock('remote pid')
-    @remote.should_receive(:is_a?).any_number_of_times.and_return{ |p|
-      [RemotePid, Pid].include?(p) } 
-    @pid = ReconnectingPid.allocate
+          
+    # We use allocate because initialize fires #connect before it is stubbed. 
+    # See init_pid helper.
+    @pid = ReconnectingPid.allocate 
     @pid.stub!(:connect).and_return(@connection)
   end
 
-  def init_pid(pid, *args)
-    pid.__send__(:initialize, *args)
+  def init_pid(overrides = {})
+    params = {:local_pid => @pid, :remote_pid => @remote }.merge(@options).merge(overrides)
+    local  = params.delete(:local_pid)
+    remote = params.delete(:remote_pid)
+    local.__send__(:initialize, remote, params)
   end
   
-  describe "with normal connection" do
-    before(:each) do
-      
-      # Expectations
-      
-      @remote.should_receive(:send).with(:quick, :brown).ordered
-      @remote.should_receive(:send).with(:fox, :jumped).ordered
-      @pid.should_receive(:connect).exactly(3).times.with(@remote).and_return{|p| @connection }
-      
-      # Scenario
-      
-      init_pid(@pid, @remote, @options)
-      @pid.send(:quick, :brown)
-      @pid.send(:fox, :jumped)
-      @pid.connection_failed(@connection)
-      @pid.connection_failed(@connection)
-      @pid.connected(@remote)
-    end
+  mock_describe "with normal connection" do
+    @remote.should_receive(:send).with(:quick, :brown).ordered
+    @remote.should_receive(:send).with(:fox, :jumped).ordered
+    @pid.should_receive(:connect).exactly(3).times.with(@remote).and_return{|p| @connection }
     
-    it "should verify mocks expectations" do
-    end
+    init_pid(:max_backlog => 2, :max_attempts => 3)
+    @pid.send(:quick, :brown)
+    @pid.send(:fox, :jumped)
+    @pid.connection_failed(@connection)
+    @pid.connection_failed(@connection)
+    @pid.connected(@remote)
   end
   
-  describe "with backlog error" do
-    before(:each) do
-
-    end
+  mock_describe "with backlog error" do
+    @remote.should_not_receive(:send)
+    @pid.should_receive(:connect).once.with(@remote).and_return{|p| @connection }
+    @pid.should_receive(:on_raise).once.with(@pid, an_instance_of(ReconnectingPid::BacklogError))
     
-    it "should verify mocks expectations" do
-      pending
-    end
+    init_pid(:max_backlog => 2)
+    @pid.send(:quick, :brown)
+    @pid.send(:fox, :jumped)
+    @pid.send(:this_should_overflow_the_backlog)
   end
 
-  describe "with exceeding max. attempts" do
-    before(:each) do
-      
-    end
+  mock_describe "with exceeding max. attempts" do
+    @remote.should_not_receive(:send)
+    @pid.should_receive(:connect).exactly(3).times.with(@remote).and_return{|p| @connection }
+    @pid.should_receive(:on_raise).once.with(@pid, an_instance_of(ReconnectingPid::AttemptsError))
     
-    it "should verify mocks expectations" do
-      pending
-    end
+    init_pid(:max_attempts => 2)
+    @pid.send(:quick, :brown)
+    @pid.connection_failed(@connection)
+    @pid.connection_failed(@connection)
   end
 
-  describe "with exceeding timeout" do
-    before(:each) do
-      
-    end
+  mock_describe "with exceeding timeout" do
+    @remote.should_not_receive(:send)
+    @pid.should_receive(:connect).exactly(3).times.with(@remote).and_return{|p| @connection }
+    @pid.should_receive(:on_raise).once.with(@pid, an_instance_of(ReconnectingPid::TimeoutError))
     
-    it "should verify mocks expectations" do
-      pending
-    end
+    init_pid(:max_attempts => 3)
+    @pid.send(:quick, :brown)
+    @pid.connection_failed(@connection)
+    @pid.connection_failed(@connection)
+    @pid.timer_action
+    @pid.timer_action
   end
-
-  
 end
--- spec/evented_api/spec_helper.rb
+++ spec/evented_api/spec_helper.rb
@@ -1,8 +1,11 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 include EventedAPI
 
-class Object
-  def scenario(*args, &blk)
-    
+module MockDescribe
+  def mock_describe(title, scope = :each, &blk)
+    describe(title) do
+      before(scope, &blk)
+      it("should verify mocks expectations") { }
+    end
   end
 end
