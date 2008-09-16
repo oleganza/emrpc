19e5a2fd3534645c7c2401a303fcfa1af972787e
Oleg Andreev <oleganza@gmail.com>
2008-09-16 15:11:36 +0200

    basic codec specs

--- lib/emrpc/util/codec.rb
+++ lib/emrpc/util/codec.rb
@@ -1,29 +1,6 @@
 module EMRPC
-  module EventedAPI    
-    #
-    # Initializes pids recursively upon any enumerable (responding to #each).
-    #
-    class ::Object
-      # DEPRECATED
-      def _initialize_pids_recursively_d4d309bd!(host_pid)
-        STDERR.puts "DEPRECATED"
-        return unless respond_to?(:each)
-        each do |*args|
-          args.each do |a|
-            a._initialize_pids_recursively_d4d309bd!(host_pid)
-          end
-        end
-      end # _initialize_pids_recursively_d4d309bd!
-    end # ::Object
-    
-    class ::String
-      def _initialize_pids_recursively_d4d309bd!(host_pid)
-        STDERR.puts "DEPRECATED"
-      end
-    end
-
-    ###############################
-    
+  module EventedAPI
+    # TODO: allow passing undumped objects throughout the system.
     class ::Object
       def encode_b381b571_1ab2_5889_8221_855dbbc76242(host_pid)
         raise "TODO: encode particular objects with recording references in the host_pid"
@@ -33,6 +10,8 @@ module EMRPC
       end
     end
     
+    # Return the primitive value as is. 
+    # It will be encoded/decoded using Marshal.dump/load methods.
     module PassThruCodec
       def encode_b381b571_1ab2_5889_8221_855dbbc76242(ref)
         self
@@ -46,7 +25,7 @@ module EMRPC
       c.send(:include, PassThruCodec)
     end
     
-    # FIXME: make in-place encoding using Enumerable#each_with_index{|e,i| }
+    # OPTIMIZE: make in-place encoding using Enumerable#each_with_index{|e,i| }
     class ::Array
       def encode_b381b571_1ab2_5889_8221_855dbbc76242(ref)
         inject([]) do |a, e|
@@ -59,7 +38,8 @@ module EMRPC
         end
       end
     end
-
+    
+    # OPTIMIZE: make in-place encoding using Hash#each{|k,v| }
     class ::Hash
       def encode_b381b571_1ab2_5889_8221_855dbbc76242(ref)
         inject({}) do |h, (k,v)|
@@ -79,13 +59,11 @@ module EMRPC
       def encode_b381b571_1ab2_5889_8221_855dbbc76242(ref)
         @marshallable_pid ||= MarshallablePid.new(@uuid)
       end
-      def _initialize_pids_recursively_d4d309bd!(host_pid)
-        STDERR.puts "DEPRECATED"
-        pid = host_pid.find_pid(@uuid)
-        initialize_with_connection(pid._connection, pid.options)
-      end
     end
     
+    # The purpose of that class is to discard all the class information about Pid before marshalling it.
+    # The only thing we want to pass around is pid.uuid.
+    #
     class MarshallablePid
       attr_accessor :uuid
       def initialize(uuid)
@@ -96,5 +74,6 @@ module EMRPC
         host_pid.find_pid(@uuid)
       end
     end
+    
   end # EventedAPI
 end # EMRPC
--- spec/util/codec_spec.rb
+++ spec/util/codec_spec.rb
@@ -1,5 +1,81 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
+describe "encode_b381b571_1ab2_5889_8221_855dbbc76242 on" do
+  
+  before(:each) do
+    @encode_method = :encode_b381b571_1ab2_5889_8221_855dbbc76242
+    @dummy_pid = mock("dummy pid")
+    @host_pid = mock("host pid", :find_pid => @dummy_pid)
+  end
+  
+  describe Object do
+    before(:each) do
+      @obj = Object.new
+    end
+    
+    it "should raise error" do
+      lambda { @obj.encode_b381b571_1ab2_5889_8221_855dbbc76242(@host_pid) }.should raise_error
+    end  
+  end
+  
+  describe "primitive", :shared => true do
+    it "should return itself" do
+      @obj.encode_b381b571_1ab2_5889_8221_855dbbc76242(@host_pid).should eql(@obj)
+    end
+  end
+
+  [nil, true, false, "string", 123, 3.725, :symbol].each do |obj|
+    describe obj.inspect do
+      before(:each) do
+        @obj = obj
+      end
+      it_should_behave_like "primitive"
+    end
+  end
+  
+  def encoding_mock(name, n)
+    enc = name.upcase.to_sym
+    item = mock(name)
+    item.stub!(@encode_method).and_return(enc)
+    item.should_receive(@encode_method).exactly(n).times.with(@host_pid).and_return do |hpid|
+      hpid.should == @host_pid
+      enc
+    end
+    item
+  end
+  
+  describe Array do
+    before(:each) do
+      item = encoding_mock("item", 6)
+      @arr = [item, item, [item, item, [item, [[item]] ]]]
+      @encoded = @arr.encode_b381b571_1ab2_5889_8221_855dbbc76242(@host_pid)
+    end
+    it "should encode each item recusively" do
+      item = :ITEM
+      @encoded.should == [item, item, [item, item, [item, [[item]] ]]]
+    end
+  end
+  
+  describe Hash do
+    before(:each) do
+      key   = encoding_mock("key", 3)
+      value = encoding_mock("value", 3)
+      @hash = { key => value, :key => { key => { "str" => value, key => {"str" => 123, :sym => value } } } }
+      @encoded = @hash.encode_b381b571_1ab2_5889_8221_855dbbc76242(@host_pid)
+    end
+    it "should encode each key and value recusively" do
+      key = :KEY
+      value = :VALUE
+      @encoded.should == { key => value, :key => { key => { "str" => value, key => {"str" => 123, :sym => value } } } }
+    end
+  end
+  
+  describe EventedAPI::Pid do
+    
+  end
+end
+
+
 # describe Object, "#_initialize_pids_recursively_d4d309bd!" do
 # 
 #   describe "without #each method" do
