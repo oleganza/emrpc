2cc70a39cdbb43825f1283186363bd03ba7f2164
Oleg Andreev <oleganza@gmail.com>
2008-09-16 18:41:30 +0200

    added working draft of blocking api test

--- spec/blocking_api_test.rb
+++ spec/blocking_api_test.rb
@@ -3,48 +3,71 @@ $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib
 require 'emrpc'
 include EMRPC
 
-module PrefixedOutput
-  def puts(line)
-    super("#{pfx}#{line}")
+PORT = 4567
+
+if child = fork # parent
+  module ServerProtocol
+    include FastMessageProtocol
+    def receive_message(data)
+      puts "Server: sending #{data.inspect} back..."
+      send_message("Re: #{data}")
+    end
   end
-  def print(line)
-    super("#{pfx}#{line}")
-  end
-  def p(line)
-    puts("#{line.inspect}")
-  end
-  def pfx
-    "#{Process.pid}: "
-  end
-end
-
-module Kernel
-  include PrefixedOutput
-end
-
-class Object
-  include PrefixedOutput
-end
-
-extend PrefixedOutput
-
-# STDOUT.extend(PrefixedOutput)
-# STDERR.extend(PrefixedOutput)
-# $stdout.extend(PrefixedOutput)
-# $stderr.extend(PrefixedOutput)
-
-if child = fork
   
-  puts "In a parent with child #{child}"
+  EM.run do
+    EM.start_server("0.0.0.0", PORT, ServerProtocol) do |conn|
+      puts "Server: connection established: #{conn}"
+    end
+  end
   
-  p [Object.new, 1]
+  puts "End of server event loop."
   
   Process.waitpid(child)
   
-else
+  puts "End of server program."
   
-  puts "In a child #{Process.pid}"
+else # child
+  EM::safe_run(:bg) { } 
+  
+  require 'thread'
+  
+  module ClientProtocol
+    include FastMessageProtocol
+    
+    def connection_completed
+      msg = "Hello!"
+      puts "connection_completed>> sending #{msg.inspect}"
+      
+      result = blocking_send_message(msg)
+      puts "Received: #{result}"
+    end
+    
+    def blocking_send_message(msg)
+      puts "blocking_send_message>> Thread.current == #{Thread.current}"
+      @msg = Queue.new
+      send_message(msg)
+      #@msg.shift
+    end
+    
+    def receive_message(data)
+      puts "receive_message>> Thread.current == #{Thread.current}"
+      puts "receive_message>> data == #{data.inspect}"
+      @msg.push(data)
+    end
+    
+    def unbind
+      puts "Client disconnected!"
+      exit
+    end
+  end
+  
   sleep 1
   
+  conn = EM.connect("localhost", PORT, ClientProtocol)
+  
+  sleep 5
+  
+  puts "End of client program."
 end
 
+puts "Very end."
